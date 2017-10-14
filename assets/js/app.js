96d62bb87c51e0ae04247004d2e6aba24fb39614
goffinet <goffinet@goffinet.org>
2017-10-14 17:53:42 +0200

    add js

---
+++ assets/js/app.js
@@ -0,0 +1,26 @@
+require.config(
+		{
+			paths	: {
+					'jquery'	: 'https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min'
+				},
+
+			shim	: {
+					'jquery'	: {
+							exports : [ 'jquery', '$' ]
+						}
+				},
+
+			baseUrl	: 'lib'
+		}
+	);
+
+
+
+require( [ '../resources/js/ui' ],
+
+	function()
+	{
+		// done!
+	}
+
+);
